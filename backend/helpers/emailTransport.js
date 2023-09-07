8af68dc69df721934baea5b920b6cbb952af7ea1
Marcin Bojara <marcin_5@mail.ru>
2023-09-07 19:46:02 +0200

    configure nodemailer email helper

---
+++ backend/helpers/emailTransport.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import nodemiler from "nodemailer";
+import { transport } from "winston";
+
+let transporter;
+if (process.env.NODE_ENV === "development") {
+  transporter = nodemiler.createTransport({
+    host: "mailhog",
+    port: 1025,
+  });
+} else if (process.env.NODE_ENV === "production") {
+  // TODO: configure mailgun in production
+}
+
+export default transporter;
