a161b7be1b8f678203bf34e09ef81d1ea23f051b
Marcin Bojara <marcin_5@mail.ru>
2023-09-13 21:11:06 +0200

    update user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import getUserProfile from "../controllers/user/getUserProfile.js";
+import updateUserProfile from "../controllers/user/updateUserProfile.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
 
 const router = express.Router();
-router.route("/profile").get(checkAuth, getUserProfile);
+router
+  .route("/profile")
+  .get(checkAuth, getUserProfile)
+  .patch(checkAuth, updateUserProfile);
 
 export default router;
