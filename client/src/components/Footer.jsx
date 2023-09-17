5c9f13ac976be4f45a6a3332e40480dd307ae145
Marcin Bojara <marcin_5@mail.ru>
2023-09-17 17:31:47 +0200

    add common ui components and hooks

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import { Box, CssBaseline, Typography } from "@mui/material";
+import { FaMoneyBillWave } from "react-icons/fa";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" align="center" sx={{ color: "#fffdfd" }}>
+      {"Copyright ©"} MERN Invoice {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+export const Footer = () => {
+  return (
+    <Box sx={{ position: "fixed", bottom: 0, width: "100%" }}>
+      <CssBaseline />
+      <Box
+        component="footer"
+        sx={{ py: 1, px: 1, mt: "auto", bgColor: "#000000" }}
+      >
+        <Typography
+          variant="subtitle1"
+          align="center"
+          component="p"
+          sx={{ color: "#07f011" }}
+        >
+          <FaMoneyBillWave>
+            Because Money is as important as oxygen{" "}
+          </FaMoneyBillWave>
+        </Typography>
+        <Copyright />
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
